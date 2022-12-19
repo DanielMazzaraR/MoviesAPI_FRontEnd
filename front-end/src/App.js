31de6548835951e0f702a02fa334d1a8b361dd0d
Daniel Mazzara <daniel.mazzara@solera.com>
2022-12-19 16:57:26 +0100

    Filter by (popularity) and (date) done --need some fixes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,12 +2,15 @@ import "./App.css";
 import axios from "axios";
 import React, { useState, useEffect, useRef } from "react";
 import CardList from "./components/CardList";
+import { getValue } from "@testing-library/user-event/dist/utils";
 
 function App() {
   const [API_CALL, setAPI_CALL] = useState();
   const api_key = "94101ad71736a755667d5010c495638a";
   const [currentPage, setCurrentPage] = useState(1);
   const title = useRef();
+  const [filter, setFilter] = useState("popularity.desc")
+
 
   const prevBtnHandler = (event) => {
     if(currentPage === 1){
@@ -35,13 +38,13 @@ function App() {
       .get(
         "https://api.themoviedb.org/3/discover/movie?api_key=" +
           api_key +
-          `&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${currentPage}&with_watch_monetization_types=flatrate`
+          `&language=en-US&sort_by=${filter}&include_adult=false&include_video=false&page=${currentPage}&with_watch_monetization_types=flatrate`
       )
       .then((respone) => {
         setAPI_CALL(respone.data.results);
         console.log(respone.data.results);
       });
-  }, [currentPage]);
+  }, [currentPage , filter]);
 
   return (
     <div className="App">
@@ -51,6 +54,12 @@ function App() {
          <h3>  {currentPage}  </h3>   
         <button onClick={nextPageHandler}> next page </button>
       </div>
+      <select value={filter} onChange={e=>{setFilter(e.target.value); setCurrentPage(1)}}>
+        <option value={"popularity.desc"}>Most popular</option>
+        <option value={"popularity.asc"}>Less popular</option>
+        <option value={"release_date.desc"}>New movies</option>
+        <option value={"release_date.asc"}>Old movies</option>
+      </select>
       <div className="cards">
         <CardList info={API_CALL} />
       </div>
--- front-end/src/components/Card.jsx
+++ front-end/src/components/Card.jsx
@@ -1,14 +1,15 @@
 import "./Card.css";
+import React, {useState} from "react";
 
 function Card(props) {
   const imgURL = "https://image.tmdb.org/t/p/w500/";
 
-  return (
+  return ( 
     <div className="card">
       <div className="face face1">
         <div className="content">
           <span className="stars"></span>
-          <h2 className="java">{props.card_info.original_title}</h2>
+          <h2 className="java">{props.card_info.title}</h2>
           <p className="java">{props.card_info.overview}</p>
           <h5>Release date: {props.card_info.release_date}</h5>
           <h5>Rating: {props.card_info.vote_average}/10</h5>
